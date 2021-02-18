57dd78396d45eb4a77ec20ef61cc271fb3e56a78
qiaozhuang <qiaozhuang1017@qq.com>
2021-02-18 22:47:10 +0800

    aa

--- app/src/main/java/com/mlink/bluetooth/gateway/adapter/SubBleDeviceAdapter.java
+++ app/src/main/java/com/mlink/bluetooth/gateway/adapter/SubBleDeviceAdapter.java
@@ -1,5 +1,6 @@
 package com.mlink.bluetooth.gateway.adapter;
 
+import android.view.View;
 import android.widget.ImageView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
@@ -18,8 +19,20 @@ public class SubBleDeviceAdapter extends BaseQuickAdapter<SubBleDevice, BaseView
     @Override
     protected void convert(@NotNull BaseViewHolder baseViewHolder, SubBleDevice subBleDevice) {
 
-        baseViewHolder.setText(R.id.sub_device_name_tv,subBleDevice.getSubId()+" ("+subBleDevice.getLevel()+")");
+        baseViewHolder.setText(R.id.sub_device_name_tv,subBleDevice.getSubId());
         ImageView imageView=baseViewHolder.itemView.findViewById(R.id.sub_device_state_iv);
+        ImageView liv=baseViewHolder.itemView.findViewById(R.id.sub_device_l_iv);
+        if (subBleDevice.getLevel()==1){
+            liv.setVisibility(View.VISIBLE);
+            liv.setImageResource(R.mipmap.leal1);
+        }else if (subBleDevice.getLevel()==2){
+            liv.setVisibility(View.VISIBLE);
+            liv.setImageResource(R.mipmap.leal2);
+        }else if (subBleDevice.getLevel()==3){
+            liv.setVisibility(View.VISIBLE);
+            liv.setImageResource(R.mipmap.leal3);
+        }
+
         if (subBleDevice.getState()==1){
             imageView.setImageResource(R.mipmap.led_2);
         }else {
