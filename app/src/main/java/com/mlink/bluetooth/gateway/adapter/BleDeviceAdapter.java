e8f3a497b44931b235fc8d7fabe1d124fd3772af
qiaozhuang <qiaozhuang1017@qq.com>
2021-02-18 20:48:38 +0800

    aa

--- app/src/main/java/com/mlink/bluetooth/gateway/adapter/BleDeviceAdapter.java
+++ app/src/main/java/com/mlink/bluetooth/gateway/adapter/BleDeviceAdapter.java
@@ -13,6 +13,8 @@ import com.mlink.bluetooth.gateway.db.SubBleManager;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+
 public
 class BleDeviceAdapter extends BaseQuickAdapter<BleDeviceInfo, BaseViewHolder> {
 
@@ -26,6 +28,16 @@ class BleDeviceAdapter extends BaseQuickAdapter<BleDeviceInfo, BaseViewHolder> {
     protected void convert(@NotNull BaseViewHolder baseViewHolder, BleDeviceInfo bleDeviceInfo) {
         baseViewHolder.setText(R.id.device_name,bleDeviceInfo.getId());
         ImageView imageView=baseViewHolder.itemView.findViewById(R.id.device_iv);
+
+        List<BleMLDevice> connectedDevices = ble.getConnectedDevices();
+        if (connectedDevices!=null&&connectedDevices.size()>0){
+            for (BleMLDevice bleMLDevice:connectedDevices){
+              if (bleMLDevice.getBleAddress().equals(bleDeviceInfo.getMacCode())){
+                  baseViewHolder.setText(R.id.device_name,bleDeviceInfo.getMacCode()+" "+"(已连接)");
+                  imageView.setImageResource(R.mipmap.bluetooth_device_1);
+              }
+            }
+        }
         if (bleDeviceInfo.getState()==1){
             baseViewHolder.setText(R.id.device_name,bleDeviceInfo.getMacCode()+" "+"(已发现)");
             imageView.setImageResource(R.mipmap.bluetooth_device_2);
